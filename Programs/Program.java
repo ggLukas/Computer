756f0c22e256f2b9d60c9655556f02f767c30d7d
SimonWiddenhielm <55542256+SimonWiddenhielm@users.noreply.github.com>
2020-04-15 15:27:07 +0200

    Update Program.java

--- Programs/Program.java
+++ Programs/Program.java
@@ -1,1 +1,24 @@
 
+import java.util.ArrayList;
+
+public abstract class Program {
+	
+	private ArrayList<Instruction> listOfInstructions= new ArrayList<>();
+	
+	
+	
+	public void add(Instruction ins ) {
+		
+		listOfInstructions.add(ins);
+		
+	}
+	public Instruction getInstruction(int count) {
+		
+		return listOfInstructions.get(count);
+		
+	}
+	
+	
+	
+	
+}
