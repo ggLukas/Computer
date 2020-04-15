f0eb435b78d41c67c3d34f42a7cd2fa5ed42d799
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 14:09:58 +0200

    Update Mul.java

--- Instructions/Mul.java
+++ Instructions/Mul.java
@@ -1,1 +1,20 @@
+package Instructions;
 
+public class Mul extends Calculation implements Instruction {
+
+	public Mul(Adress adress, Operand operand1, Operand operand2) {
+		super(adress,operand1,operand2);
+	}
+	
+
+	public void performCalc(Word res, Word word1, Word word2) {
+		res.multiply(word1,word2);
+		
+	}
+
+	public String getOpName() {
+		return "Mul";
+		
+	}
+	
+}
