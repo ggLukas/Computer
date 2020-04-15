9e734d12638ff629cfc01fb96db1424ed0540da2
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 14:07:28 +0200

    Update Add.java

--- Instructions/Add.java
+++ Instructions/Add.java
@@ -1,1 +1,21 @@
+package Instructions;
+
+public class Add extends Calculation implements Instruction {
+
+	public Add(Adress adress, Operand operand1, Operand operand2) {
+		super(adress,operand1,operand2);
+	}
+	
+
+	public void performCalc(Word res, Word word1, Word word2) {
+		res.add(word1,word2);
+		
+	}
+
+	public String getOpName() {
+		return "Add";
+		
+	}
+	
+}
 
