379c10a5d421aa867d38403fdc40fac5fe9f2566
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 17:27:24 +0200

    Update Copy.java

--- Instructions/Copy.java
+++ Instructions/Copy.java
@@ -1,18 +1,27 @@
 package Instructions;
+import computer.*;
 
 public class Copy implements Instruction {
 	private Operand operand;
-	private Adress adress;
+	private Address address;
 	
 	public Copy(Operand operand, Address address) {
 		this.operand = operand;
-		this.adress = adress;
+		this.address = address;
 	}
 
+	@Override
 	public void execute(Memory mem, ProgramCounter pc) {
 		Word word = mem.getWord(address);
 		
+		if(word == null) {
+			word = new LongWord(0);
+			mem.setWord(word, address);
+		}
+		
 		word.set(operand,mem);
+		
+		//word.set(operand.getWord(mem).getValue());
 		
 		
 	}
