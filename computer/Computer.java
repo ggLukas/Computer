cc4b9e121efd84193548060aa5f783d362dece05
SimonWiddenhielm <55542256+SimonWiddenhielm@users.noreply.github.com>
2020-04-15 15:40:31 +0200

    EIN COMPUUUTER

--- computer/Computer.java
+++ computer/Computer.java
@@ -1,1 +1,35 @@
+import Programs.*;
 
+public class Computer {
+	
+	
+	private Memory mem;
+	private Program prog;
+	private ProgramCounter pCount;
+	
+	public Computer(Memory mem) {
+		this.mem=mem;
+		pCount = new ProgramCounter();
+		
+	}
+
+	public void load(Program prog) {
+		this.prog=prog;
+		
+	}
+	public void run() {
+		
+		while(pCount.isRunning()) {
+			
+			
+			prog.getInstruction(pCount.getCount());
+			pCount.next();
+			
+			
+			
+		}
+		System.out.println("The program is finished");
+		
+		
+	}
+}
