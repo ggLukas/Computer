5b72a08f965caa873d94e3dda6a767344aacd730
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 15:02:30 +0200

    Update Copy.java

--- Instructions/Copy.java
+++ Instructions/Copy.java
@@ -1,1 +1,20 @@
+package Instructions;
 
+public class Copy implements Instruction {
+	private Operand operand;
+	private Address address;
+	
+	public Copy(Operand operand, Address address) {
+		this.operand = operand;
+		this.address = address;
+	}
+
+	public void execute(Memory mem, ProgramCounter pc) {
+		Word word = mem.getWord(address);
+		
+		word.set(operand,mem);
+		
+		
+	}
+
+}
