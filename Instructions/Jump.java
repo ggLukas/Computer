07b96c761f8a58111a0be821e40070d3854fa20a
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 14:17:29 +0200

    Update Jump.java

--- Instructions/Jump.java
+++ Instructions/Jump.java
@@ -1,1 +1,16 @@
+package Instructions;
 
+public class Jump implements Instruction {
+	
+	private int index;
+	
+	public Jump(int index) {
+		this.index = index;
+	}
+
+	public void execute(Memory mem, ProgramCounter pc) {
+		pc.setIndex(index);
+		
+	}
+
+}
