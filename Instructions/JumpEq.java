f6322a5090e6cc0cd2732050529b8f04e12f3942
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 14:37:17 +0200

    Update JumpEq.java

--- Instructions/JumpEq.java
+++ Instructions/JumpEq.java
@@ -1,1 +1,23 @@
+package Instructions;
 
+public class JumpEq implements Instruction {
+	
+	private Operand operand1;
+	private Operand operand2;
+	private int index;
+	
+	public JumpEq(int index, Operand operand1, Operand operand2) {
+		this.index = index;
+		this.operand1 = operand1;
+		this.operand2 = operand2;
+	}
+	
+
+	public void execute(Memory mem, ProgramCounter pc) {
+		if(operand1.getWord(mem).getValue().equals(operand2.getWord(mem).getValue())) {
+			pc.setIndex(index);
+		}
+		
+	}
+	
+}
