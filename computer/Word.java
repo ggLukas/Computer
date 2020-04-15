58a7fbeb7b13640f7b880bdee1a637cab49cbc53
marcussjokvist <55698855+marcussjokvist@users.noreply.github.com>
2020-04-15 15:40:52 +0200

    Update Word.java

--- computer/Word.java
+++ computer/Word.java
@@ -1,1 +1,27 @@
+package computer;
 
+public abstract class Word implements Operand {
+	private long value;
+	
+	public abstract Word getWord(Memory memory);
+	
+	public long getValue() {
+		return value;
+	}
+	
+	public void set(long longen) {
+		value = longen;
+	}
+	
+	public void add(Word w1, Word w2) {
+		value = w1.getValue() + w2.getValue();
+	}
+	
+	public boolean equals(Word w) {
+		return this.equals(w);
+	}
+	
+	public void mul(Word w1, Word w2) {
+		value = w1.getValue() * w2.getValue();
+	}
+}
