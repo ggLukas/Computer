955554d56beb47c58d504fb24206d66f8e8567da
marcussjokvist <55698855+marcussjokvist@users.noreply.github.com>
2020-04-15 15:41:11 +0200

    Update LongWord.java

--- computer/LongWord.java
+++ computer/LongWord.java
@@ -1,1 +1,17 @@
+package computer;
 
+public class LongWord extends Word {
+	private long value;
+	
+	public LongWord(long value) {
+		this.value = value;
+	}
+	
+	public Word getWord(Memory memory) {
+		return this;
+	}
+	
+	public String toString() {
+    	return String.valueOf(this.value);
+    }
+}
