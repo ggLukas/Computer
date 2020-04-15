6b3f2d877d4362fd5a4ad45da88176ea22988d89
SimonWiddenhielm <55542256+SimonWiddenhielm@users.noreply.github.com>
2020-04-15 14:32:49 +0200

    Update Memory.java
    The shit is lit

--- computer/Memory.java
+++ computer/Memory.java
@@ -1,1 +1,17 @@
 
+
+public abstract class Memory {
+	
+	private Word[]words;
+	
+	public Memory(int n) {
+		words = new Word[n];
+	}
+	public void setWord(Word word, Address address) {
+		words[address.getIndex()] = word;
+	}
+	public Word getWord(Adress adress) {
+		return words[adress.getIndex()];
+	}
+
+}
