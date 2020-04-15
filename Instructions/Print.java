1e3e9ee3ad7cab350d6ef0f11a47d59017b06289
ggLukas <59331147+ggLukas@users.noreply.github.com>
2020-04-15 14:52:25 +0200

    Update Print.java

--- Instructions/Print.java
+++ Instructions/Print.java
@@ -1,1 +1,15 @@
+package Instructions;
 
+public class Print implements Instruction {
+	private Operand operand;
+	
+	public Print(Operand operand) {
+		
+	}
+
+	public void execute(Memory mem, ProgramCounter pc) {
+		System.out.println(operand.getWord(mem));
+		
+	}
+
+}
